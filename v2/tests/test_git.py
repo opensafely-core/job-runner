08d1591355b6b299b0aa9b7a591410220f6009f5
David Evans <dave.evans@thedatalab.org>
2020-10-25 15:50:30 +0000

    Add faster, local-only git test option

--- v2/tests/test_git.py
+++ v2/tests/test_git.py
@@ -1,8 +1,18 @@
 import os
+from pathlib import Path
 
 import pytest
 
 from jobrunner.git import read_file_from_repo, checkout_commit, get_sha_from_remote_ref
+
+
+REPO_FIXTURE = str(Path(__file__).parent.resolve() / "fixtures/git-repo")
+
+
+remote_repo_test = pytest.mark.skipif(
+    bool(os.environ.get("LOCAL_TESTS_ONLY")),
+    reason="Skipping tests which talk to GitHub",
+)
 
 
 @pytest.fixture()
@@ -10,6 +20,7 @@ def setup_config(tmp_path, monkeypatch):
     monkeypatch.setattr("jobrunner.config.GIT_REPO_DIR", tmp_path)
 
 
+@remote_repo_test
 def test_read_file_from_repo(setup_config):
     output = read_file_from_repo(
         "https://github.com/opensafely/documentation.git",
@@ -19,6 +30,7 @@ def test_read_file_from_repo(setup_config):
     assert output == b"# documentation"
 
 
+@remote_repo_test
 def test_checkout_commit(setup_config, tmp_path):
     target_dir = tmp_path / "files"
     checkout_commit(
@@ -29,6 +41,7 @@ def test_checkout_commit(setup_config, tmp_path):
     assert [f.name for f in target_dir.iterdir()] == ["README.md"]
 
 
+@remote_repo_test
 def test_get_sha_from_remote_ref():
     sha = get_sha_from_remote_ref(
         "https://github.com/opensafely/cohort-extractor", "v1.0.0"
@@ -40,6 +53,7 @@ def test_get_sha_from_remote_ref():
     not os.environ.get("PRIVATE_REPO_ACCESS_TOKEN"),
     reason="No access token in environment",
 )
+@remote_repo_test
 def test_read_file_from_private_repo(setup_config):
     output = read_file_from_repo(
         "https://github.com/opensafely/test-repository.git",
@@ -47,3 +61,27 @@ def test_read_file_from_private_repo(setup_config):
         "README.md",
     )
     assert output == b"# test-repository\nTesting GH permssions model\n"
+
+
+# The below tests use a local git repo fixture rather than accessing GitHub
+# over HTTPS. This makes them faster, though obviously less complete.
+
+
+def test_read_file_from_repo_local(setup_config):
+    output = read_file_from_repo(
+        REPO_FIXTURE, "d1e88b31cbe8f67c58f938adb5ee500d54a69764", "project.yaml",
+    )
+    assert output.startswith(b"version: '1.0'")
+
+
+def test_checkout_commit_local(setup_config, tmp_path):
+    target_dir = tmp_path / "files"
+    checkout_commit(
+        REPO_FIXTURE, "d1e88b31cbe8f67c58f938adb5ee500d54a69764", target_dir,
+    )
+    assert [f.name for f in target_dir.iterdir()] == ["project.yaml"]
+
+
+def test_get_sha_from_remote_ref_local():
+    sha = get_sha_from_remote_ref(REPO_FIXTURE, "v1")
+    assert sha == b"d1e88b31cbe8f67c58f938adb5ee500d54a69764"
