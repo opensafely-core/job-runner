d7fe29b8dba63f57bfb37b1934ae3072b3d17325
David Evans <dave.evans@thedatalab.org>
2020-10-25 15:53:05 +0000

    Make fixture "autouse" to reduce duplication

--- v2/tests/test_git.py
+++ v2/tests/test_git.py
@@ -15,13 +15,13 @@ remote_repo_test = pytest.mark.skipif(
 )
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def setup_config(tmp_path, monkeypatch):
     monkeypatch.setattr("jobrunner.config.GIT_REPO_DIR", tmp_path)
 
 
 @remote_repo_test
-def test_read_file_from_repo(setup_config):
+def test_read_file_from_repo():
     output = read_file_from_repo(
         "https://github.com/opensafely/documentation.git",
         "e28665e3796841c6b42f995f9de28f7177ec5e91",
@@ -31,7 +31,7 @@ def test_read_file_from_repo(setup_config):
 
 
 @remote_repo_test
-def test_checkout_commit(setup_config, tmp_path):
+def test_checkout_commit(tmp_path):
     target_dir = tmp_path / "files"
     checkout_commit(
         "https://github.com/opensafely/documentation.git",
@@ -54,7 +54,7 @@ def test_get_sha_from_remote_ref():
     reason="No access token in environment",
 )
 @remote_repo_test
-def test_read_file_from_private_repo(setup_config):
+def test_read_file_from_private_repo():
     output = read_file_from_repo(
         "https://github.com/opensafely/test-repository.git",
         "d7fe87ab5d6dc97222c4a9dbf7c0fe40fc108c8f",
@@ -67,14 +67,14 @@ def test_read_file_from_private_repo(setup_config):
 # over HTTPS. This makes them faster, though obviously less complete.
 
 
-def test_read_file_from_repo_local(setup_config):
+def test_read_file_from_repo_local():
     output = read_file_from_repo(
         REPO_FIXTURE, "d1e88b31cbe8f67c58f938adb5ee500d54a69764", "project.yaml",
     )
     assert output.startswith(b"version: '1.0'")
 
 
-def test_checkout_commit_local(setup_config, tmp_path):
+def test_checkout_commit_local(tmp_path):
     target_dir = tmp_path / "files"
     checkout_commit(
         REPO_FIXTURE, "d1e88b31cbe8f67c58f938adb5ee500d54a69764", target_dir,
