1264aa2ee2baa6c5bf0da0ec55a0d03913f42a76
David Evans <dave.evans@thedatalab.org>
2020-11-06 22:27:39 +0000

    Locate path-based config together

--- v2/jobrunner/config.py
+++ v2/jobrunner/config.py
@@ -13,18 +13,6 @@ GIT_REPO_DIR = WORK_DIR / "repos"
 
 DATABASE_FILE = WORK_DIR / "db.sqlite"
 
-PRIVATE_REPO_ACCESS_TOKEN = os.environ.get("PRIVATE_REPO_ACCESS_TOKEN", "")
-
-JOB_LOOP_INTERVAL = float(os.environ.get("JOB_LOOP_INTERVAL", "0.5"))
-
-BACKEND = os.environ.get("BACKEND", "expectations")
-
-USING_DUMMY_DATA_BACKEND = BACKEND == "expectations"
-
-ALLOWED_IMAGES = {"cohortextractor", "stata-mp", "r", "jupyter"}
-
-DOCKER_REGISTRY = "docker.opensafely.org"
-
 HIGH_PRIVACY_STORAGE_BASE = Path(
     os.environ.get("HIGH_PRIVACY_STORAGE_BASE", WORK_DIR / "high_privacy")
 )
@@ -36,6 +24,18 @@ HIGH_PRIVACY_WORKSPACES_DIR = HIGH_PRIVACY_STORAGE_BASE / "workspaces"
 MEDIUM_PRIVACY_WORKSPACES_DIR = MEDIUM_PRIVACY_STORAGE_BASE / "workspaces"
 
 JOB_LOG_DIR = HIGH_PRIVACY_STORAGE_BASE / "logs"
+
+PRIVATE_REPO_ACCESS_TOKEN = os.environ.get("PRIVATE_REPO_ACCESS_TOKEN", "")
+
+JOB_LOOP_INTERVAL = float(os.environ.get("JOB_LOOP_INTERVAL", "0.5"))
+
+BACKEND = os.environ.get("BACKEND", "expectations")
+
+USING_DUMMY_DATA_BACKEND = BACKEND == "expectations"
+
+ALLOWED_IMAGES = {"cohortextractor", "stata-mp", "r", "jupyter"}
+
+DOCKER_REGISTRY = "docker.opensafely.org"
 
 DATABASE_URLS = {
     "full": os.environ.get("FULL_DATABASE_URL"),
