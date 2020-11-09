6b58ff77e603ce640fe24508a1aedcf986f21697
David Evans <dave.evans@thedatalab.org>
2020-11-09 12:33:16 +0000

    Refactor logging

---
+++ v2/jobrunner/log_utils.py
@@ -0,0 +1,106 @@
+import contextlib
+import logging
+import os
+import subprocess
+import sys
+import threading
+
+
+class JobRunnerFormatter(logging.Formatter):
+    def format(self, record):
+        """
+        Set the `context` record attribute to a string giving the current job
+        or job request we're processing
+        """
+        if hasattr(record, "job"):
+            context = f"job#{record.job.id}: "
+        elif hasattr(record, "job_request"):
+            context = f"job_request#{record.job_request.id}: "
+        else:
+            context = ""
+        record.context = context
+        return super().format(record)
+
+    def formatException(self, exc_info):
+        """
+        We frequently shell out to docker and git and it's very useful to have
+        the stderr output in the logs rather than just "this process failed"
+        """
+        message = super().formatException(exc_info)
+        value = exc_info[1]
+        if isinstance(value, subprocess.CalledProcessError):
+            stderr = value.stderr
+            if stderr:
+                if isinstance(stderr, bytes):
+                    stderr = stderr.decode("utf-8", "ignore")
+                message = f"{message}\n\nstderr:\n\n{stderr}"
+        return message
+
+
+def show_subprocess_stderr(typ, value, traceback):
+    """
+    This applies the same CalledProcessError formatting as in `formatException`
+    above but to uncaught exceptions
+    """
+    sys.__excepthook__(typ, value, traceback)
+    if isinstance(value, subprocess.CalledProcessError):
+        stderr = value.stderr
+        if stderr:
+            if isinstance(stderr, bytes):
+                stderr = stderr.decode("utf-8", "ignore")
+            print("\nstderr:\n", file=sys.stderr)
+            print(stderr, file=sys.stderr)
+
+
+class SetLogContext(threading.local):
+    """
+    A context manager which allows setting `extra` values on all logging calls
+    which occur anywhere in its context e.g.
+
+        set_log_context = SetLogContext()
+        with set_log_context(foo="bar"):
+            log.info("hello world")
+
+    Will result in the equivalent log message to:
+
+        log.info("hello word", extra={"foo": "bar"})
+
+    Uses threading.local for thread safety.
+    """
+
+    def __init__(self):
+        self.current_context = {}
+        self.context_stack = []
+
+    @contextlib.contextmanager
+    def __call__(self, **kwargs):
+        """
+        Create a new logging context with the supplied keyword arguments (in
+        addition to any inherited from the current context)
+        """
+        self.context_stack.append(self.current_context)
+        self.current_context = dict(self.current_context, **kwargs)
+        try:
+            yield
+        finally:
+            self.current_context = self.context_stack.pop()
+
+    def filter(self, record):
+        """
+        Apply the current context to a LogRecord
+        """
+        for key, value in self.current_context.items():
+            if not hasattr(record, key):
+                setattr(record, key, value)
+        return True
+
+
+set_log_context = SetLogContext()
+
+
+def configure_logging():
+    handler = logging.StreamHandler()
+    handler.addFilter(set_log_context)
+    handler.setFormatter(JobRunnerFormatter("{context}{message}", style="{"))
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"), handlers=[handler])
+    sys.excepthook = show_subprocess_stderr
--- v2/jobrunner/run.py
+++ v2/jobrunner/run.py
@@ -1,6 +1,8 @@
 import datetime
+import logging
 import time
 
+from .log_utils import configure_logging, set_log_context
 from . import config
 from .database import find_where, count_where, update, select_values
 from .models import Job, State
@@ -10,8 +12,10 @@ from .manage_jobs import (
     job_still_running,
     finalise_job,
     cleanup_job,
-    job_slug,
 )
+
+
+log = logging.getLogger(__name__)
 
 
 def main(exit_when_done=False):
@@ -25,10 +29,11 @@ def main(exit_when_done=False):
 def handle_jobs():
     jobs = find_where(Job, status__in=[State.PENDING, State.RUNNING])
     for job in jobs:
-        if job.status == State.PENDING:
-            handle_pending_job(job)
-        elif job.status == State.RUNNING:
-            handle_running_job(job)
+        with set_log_context(job=job):
+            if job.status == State.PENDING:
+                handle_pending_job(job)
+            elif job.status == State.RUNNING:
+                handle_running_job(job)
     return len(jobs)
 
 
@@ -38,10 +43,10 @@ def handle_pending_job(job):
         mark_job_as_failed(job, JobError("Not starting as dependency failed"))
     elif all(state == State.COMPLETED for state in awaited_states):
         if not job_running_capacity_available():
-            log(job, "Waiting for available workers", timestamp=True)
+            set_message(job, "Waiting for available workers", timestamp=True)
         else:
             try:
-                log(job, "Starting")
+                set_message(job, "Starting")
                 start_job(job)
             except JobError as e:
                 mark_job_as_failed(job, e)
@@ -49,15 +54,15 @@ def handle_pending_job(job):
             else:
                 mark_job_as_running(job)
     else:
-        log(job, "Waiting on dependencies", timestamp=True)
+        set_message(job, "Waiting on dependencies", timestamp=True)
 
 
 def handle_running_job(job):
     if job_still_running(job):
-        log(job, "Running", timestamp=True)
+        set_message(job, "Running", timestamp=True)
     else:
         try:
-            log(job, "Finished, copying outputs")
+            set_message(job, "Finished, copying outputs")
             finalise_job(job)
         except JobError as e:
             mark_job_as_failed(job, e)
@@ -78,21 +83,21 @@ def mark_job_as_failed(job, exception):
     job.status = State.FAILED
     job.status_message = f"{type(exception).__name__}: {exception}"
     update(job, update_fields=["status", "status_message"])
-    display(job)
+    log.info(job.status_message)
 
 
 def mark_job_as_running(job):
     job.status = State.RUNNING
     job.status_message = "Started"
     update(job, update_fields=["status", "status_message"])
-    display(job)
+    log.info(job.status_message)
 
 
 def mark_job_as_completed(job):
     job.status = State.COMPLETED
     job.status_message = "Completed successfully"
     update(job, update_fields=["status", "status_message"])
-    display(job)
+    log.info(job.status_message)
 
 
 def job_running_capacity_available():
@@ -100,21 +105,21 @@ def job_running_capacity_available():
     return running_jobs < config.MAX_WORKERS
 
 
-def log(job, message, timestamp=False):
+def set_message(job, message, timestamp=False):
     # A bit of a hack, but hopefully worthwhile: there are certain states
-    # (waiting and running) which we can expect some jobs to stay in a for a
-    # long time. We don't want to spam the logs (or the database) by writing
+    # (waiting and running) which we can expect some jobs to stay in for quite
+    # a long time. We don't want to spam the logs (or the database) by writing
     # these out everytime. But if we only write them when they change then we
     # may have to look a long way back in the logs to find the last update, and
     # it's harder for users to have confidence that the job really is still
-    # running or waiting. By appending a timestamp and ignoring the final
-    # character when checking for changes we end up only logging the state
-    # every 10 minutes which seems about right.
+    # running or waiting. By appending a timestamp and ignoring the final digit
+    # when checking for changes we end up only logging the state every 10
+    # minutes which seems about right.
     if timestamp:
         now = datetime.datetime.now(datetime.timezone.utc)
-        message = f"{message} at {now:%Y-%m-%d %H:%M}"
+        message = f"{message} at {now:%Y-%m-%d %H:%M} UTC"
         if job.status_message:
-            changed = job.status_message[:-1] != message[:-1]
+            changed = job.status_message[:-5] != message[:-5]
         else:
             changed = True
     else:
@@ -122,12 +127,9 @@ def log(job, message, timestamp=False):
     if changed:
         job.status_message = message
         update(job, update_fields=["status_message"])
-        display(job)
-
-
-def display(job):
-    print(f"Job #{job_slug(job)}: {job.status_message}")
+        log.info(job.status_message)
 
 
 if __name__ == "__main__":
+    configure_logging()
     main()
