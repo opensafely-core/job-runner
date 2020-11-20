56c114e915b5ae72a95f7f9ae9718097ab1c67ed
David Evans <dave.evans@thedatalab.org>
2020-11-20 17:27:18 +0000

    Switch to using a partial index on status
    As the set of active jobs will always be relatively small this keeps the
    index size nicely bounded.

--- jobrunner/schema.sql
+++ jobrunner/schema.sql
@@ -37,4 +37,9 @@ CREATE TABLE job (
 
 CREATE INDEX idx_job__job_request_id ON job (job_request_id);
 
-CREATE INDEX idx_job__status ON job (status);
+-- Once jobs transition into a terminal state (failed or succeeded) they become
+-- basically irrelevant from the application's point of view as it never needs
+-- to query them. By creating an index only on non-terminal states we ensure
+-- that it always stays relatively small even as the set of historical jobs
+-- grows.
+CREATE INDEX idx_job__status ON job (status) WHERE status NOT IN ('F', 'S');
