1b25d5181d0192569f6b04b9d5b8788fa71a2216
bloodearnest <bloodearnest@gmail.com>
2020-12-17 17:21:56 +0000

    do not run the test on windows as it's not worth it

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,19 +1,24 @@
 from textwrap import dedent
+import platform
 import signal
 import subprocess
 import sys
 import time
 
+import pytest
+
 from jobrunner import service
 from jobrunner.subprocess_utils import subprocess_run
 
 
+@pytest.mark.skipif(
+    platform.system() == "Windows", reason="tricky to do ctrl-c in windows"
+)
 def test_service_main():
     p = subprocess.Popen([sys.executable, "-m", "jobrunner.service"])
     assert p.returncode is None
     time.sleep(3)
-    # windows needs a different signal
-    p.send_signal(getattr(signal, 'CTRL_C_EVENT', signal.SIGINT))
+    p.send_signal(signal.SIGINT)
     p.wait()
     assert p.returncode == 0
 
