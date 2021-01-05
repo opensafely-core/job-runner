c55e68103e02d74c4557480f272ac7af0606e42b
bloodearnest <bloodearnest@gmail.com>
2021-01-05 17:27:24 +0000

    fix: make windows service use a bash runner
    This changes the nssm config to launch a bash script that sources .env
    and then execs to python.
    
    This means we do not need to do any env parsing in python, which is
    good.
    
    Also, we drop the OPENSAFELY_ prefix used by the current .env and
    docker-compose tooling, just using the actual environment variables
    directly.

--- jobrunner/service.py
+++ jobrunner/service.py
@@ -9,37 +9,14 @@ import sys
 import time
 import threading
 
-log = logging.getLogger(__name__)
-
-# temp workaround for not having PYTHONPATH in the service config
-sys.path.insert(0, "./lib")
-
-# load any env file *before* we import config module
-# TODO: we should probably have a wrapper that does this before python is
-# loaded, or make config reloadable.
-def parse_env(contents):
-    """Parse a simple environment file."""
-    env = {}
-    for line in contents.split("\n"):
-        line = line.strip()
-        if not line or line[0] == "#":
-            continue
-        k, _, v = line.partition("=")
-        env[k.strip()] = v.strip().strip('"').strip("'")
-    return env
-
-
-path = Path(os.environ.get("ENVPATH", ".env"))
-if path.exists():
-    log.info(f"Loading environment variables from {path}")
-    env = parse_env(path.read_text())
-    if env:
-        os.environ.update(env)
 
 from . import config
 from .log_utils import configure_logging
 from . import run
 from . import sync
+
+
+log = logging.getLogger(__name__)
 
 
 def main():
--- scripts/install.bat
+++ scripts/install.bat
@@ -1,20 +1,22 @@
 REM usage: install.bat USER PASSWORD
-nssm install opensafely "C:\Program Files\Python39\python" -m jobrunner.service
-nssm set opensafely AppDirectory E:\job-runner
+REM installs/configures the opensafely service
+set directory="E:\job-runner"
+nssm stop opensafely
+nssm remove opensafely confirm
+nssm install opensafely "C:\Program Files\Git\usr\bin\bash" "%directory%\scripts\run.sh"
+nssm set opensafely AppDirectory %directory%
 nssm set opensafely DisplayName "OpenSAFELY job runner"
 nssm set opensafely Start SERVICE_AUTO_START
-REM Note: use AppEnvironmentExtra to avoid any potential conflicts with registry based AppEnvironment key
-nssm set opeensafely AppEnvironmentExtra PYTHONPATH=lib
 REM login as user
-nssm set opensafely ObjectName %1% %2%
+nssm set opensafely ObjectName %1 %2
 nssm set opensafely DependOnService com.docker.service
 nssm set opensafely AppThrottle 10000
 nssm set opensafely AppExit Default Restart
 nssm set opensafely AppRestartDelay 1000
-# don't send WM_CLOSE or WM_QUIT, but do send Ctrl-C and TerminateProcess
+REM don't send WM_CLOSE or WM_QUIT, but do send Ctrl-C and TerminateProcess
 nssm set opensafely AppStopMethodSkip 6
-nssm set opensafely AppStdout E:\job-runner\service.log
-nssm set opensafely AppStderr E:\job-runner\service.err.log
+nssm set opensafely AppStdout %directory%\service.log
+nssm set opensafely AppStderr %directory%\service.err.log
 nssm set opensafely AppStdoutCreationDisposition 4
 nssm set opensafely AppStderrCreationDisposition 4
 nssm set opensafely AppRotateFiles 1
