afc6dc1ac0d2c50046b95f24fe70dc9ed7ffd084
David Evans <dave.evans@thedatalab.org>
2021-03-15 15:54:53 +0000

    feat: Add crude service to log system stats to a SQLite file

--- jobrunner/config.py
+++ jobrunner/config.py
@@ -134,3 +134,10 @@ def parse_job_resource_weights(config_file):
 
 
 JOB_RESOURCE_WEIGHTS = parse_job_resource_weights("job-resource-weights.ini")
+
+
+STATS_DATABASE_FILE = os.environ.get("STATS_DATABASE_FILE")
+if STATS_DATABASE_FILE:
+    STATS_DATABASE_FILE = Path(STATS_DATABASE_FILE)
+
+STATS_POLL_INTERVAL = float(os.environ.get("STATS_POLL_INTERVAL", "10"))
---
+++ jobrunner/record_stats.py
@@ -0,0 +1,80 @@
+"""
+Super crude docker/system stats logger
+"""
+import datetime
+import json
+import logging
+import sqlite3
+import sys
+import time
+
+from . import config
+from .docker_stats import get_container_stats, get_volume_and_container_sizes
+from .system_stats import get_system_stats
+from .log_utils import configure_logging
+
+
+SCHEMA_SQL = """
+CREATE TABLE stats (
+    timestamp TEXT,
+    data TEXT
+);
+"""
+
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    database_file = config.STATS_DATABASE_FILE
+    if not database_file:
+        log.info("STATS_DATABASE_FILE not set; not polling for system stats")
+        return
+    log.info(f"Logging system stats to: {database_file}")
+    connection = get_database_connection(database_file)
+    while True:
+        log_stats(connection)
+        time.sleep(config.STATS_POLL_INTERVAL)
+
+
+def get_database_connection(filename):
+    filename.parent.mkdir(parents=True, exist_ok=True)
+    conn = sqlite3.connect(filename)
+    # Enable autocommit
+    conn.isolation_level = None
+    schema_count = list(conn.execute("SELECT COUNT(*) FROM sqlite_master"))[0][0]
+    if schema_count == 0:
+        conn.executescript(SCHEMA_SQL)
+    return conn
+
+
+def log_stats(connection):
+    stats = get_all_stats()
+    # If no containers are running then don't log anything
+    if not stats["containers"]:
+        return
+    timestamp = datetime.datetime.utcnow().isoformat()
+    connection.execute(
+        "INSERT INTO stats (timestamp, data) VALUES (?, ?)",
+        [timestamp, json.dumps(stats)],
+    )
+
+
+def get_all_stats():
+    stats = get_system_stats()
+    volume_sizes, container_sizes = get_volume_and_container_sizes()
+    containers = get_container_stats()
+    for name, container in containers.items():
+        container["disk_used"] = container_sizes.get(name)
+    stats["containers"] = containers
+    stats["volumes"] = volume_sizes
+    return stats
+
+
+if __name__ == "__main__":
+    configure_logging()
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
