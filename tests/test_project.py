de56c83f93ed10c39bf202653c7a0db1cd5cdddc
bloodearnest <bloodearnest@gmail.com>
2021-04-12 11:40:19 +0100

    feat: Add validation for outputs_for_publishing field
    Proposed new field here:
    
    https://github.com/opensafely-core/roadmap/discussions/11
    
    This simply validates the field, but it is optional, so should be
    backwards compatible

--- tests/test_project.py
+++ tests/test_project.py
@@ -4,6 +4,7 @@ from jobrunner.project import (
     parse_and_validate_project_file,
     ProjectValidationError,
     assert_valid_glob_pattern,
+    assert_valid_published_output,
     InvalidPatternError,
 )
 
@@ -35,3 +36,31 @@ def test_assert_valid_glob_pattern():
     for pattern in bad_patterns:
         with pytest.raises(InvalidPatternError):
             assert_valid_glob_pattern(pattern)
+
+
+@pytest.mark.parametrize(
+    "info,msg",
+    [
+        ({}, "missing field 'type'"),
+        ({"type": "table"}, "missing field 'files'"),
+        ({"type": "table", "files": []}, "missing field 'description'"),
+        (
+            {"type": "INVALID", "files": [], "description": "description"},
+            "invalid type of 'INVALID'",
+        ),
+        (
+            {"type": "table", "files": ["highly.csv"], "description": "description"},
+            "file 'highly.csv' does not match a moderately_sensitive action output.",
+        ),
+        (
+            {"type": "notebook", "files": ["output.csv"], "description": "description"},
+            "file 'output.csv' has invalid extension for output type 'notebook'",
+        ),
+    ],
+)
+def test_assert_valid_published_output(info, msg):
+    moderate_outputs = {"output.csv"}
+    with pytest.raises(ProjectValidationError) as err:
+        assert_valid_published_output("name", info, moderate_outputs)
+
+    assert str(err.value).startswith("outputs_for_publication name: " + msg)
