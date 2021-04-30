5d6119edf5ec034aee256c4d81eb223cc20e03e6
David Evans <dave.evans@thedatalab.org>
2021-04-30 14:22:09 +0100

    Support checking if commit is reachable from a given ref

--- tests/test_git.py
+++ tests/test_git.py
@@ -3,7 +3,12 @@ from pathlib import Path
 
 import pytest
 
-from jobrunner.git import read_file_from_repo, checkout_commit, get_sha_from_remote_ref
+from jobrunner.git import (
+    read_file_from_repo,
+    checkout_commit,
+    get_sha_from_remote_ref,
+    commit_reachable_from_ref,
+)
 
 
 REPO_FIXTURE = str(Path(__file__).parent.resolve() / "fixtures/git-repo")
@@ -52,6 +57,22 @@ def test_read_file_from_private_repo(tmp_work_dir):
         "README.md",
     )
     assert output == b"# test-repository\nTesting GH permssions model\n"
+
+
+@pytest.mark.slow_test
+def test_commit_reachable_from_ref(tmp_work_dir):
+    is_reachable_good = commit_reachable_from_ref(
+        "https://github.com/opensafely/documentation",
+        "2170fadfeca7baf04f50376677253513e0a12bb1",
+        "1.6.0",
+    )
+    assert is_reachable_good
+    is_reachable_bad = commit_reachable_from_ref(
+        "https://github.com/opensafely/documentation",
+        "afaee06237df52d78aa85920248572d5803379a3",
+        "1.6.0",
+    )
+    assert not is_reachable_bad
 
 
 # The below tests use a local git repo fixture rather than accessing GitHub
