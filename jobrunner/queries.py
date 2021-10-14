cc504543e8256e0d7bfafe1b2bf00e779957309b
Ben Butler-Cole <ben@bridesmere.com>
2021-10-14 12:10:08 +0100

    fix: Don't consider dummy __error_ jobs to be part of workspace state

--- jobrunner/queries.py
+++ jobrunner/queries.py
@@ -8,11 +8,15 @@ from jobrunner.models import Job
 def calculate_workspace_state(workspace):
     """
     Return a list containing the most recent uncancelled job (if any) for each action in the workspace. We always
-    ignore cancelled jobs when considering the historical state of the system.
+    ignore cancelled jobs when considering the historical state of the system. We also ignore jobs whose action is
+    '__error__'; these are dummy jobs created only to help us communicate failure states back to the job-server (see
+    create_or_update_jobs.create_failed_job()).
     """
     all_jobs = find_where(Job, workspace=workspace, cancelled=False)
     latest_jobs = []
-    for _, jobs in group_by(all_jobs, attrgetter("action")):
+    for action, jobs in group_by(all_jobs, attrgetter("action")):
+        if action == "__error__":
+            continue
         ordered_jobs = sorted(jobs, key=attrgetter("created_at"), reverse=True)
         latest_jobs.append(ordered_jobs[0])
     return latest_jobs
