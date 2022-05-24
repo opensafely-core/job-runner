667c493e602102141ef60bc98e46e7387e473aff
bloodearnest <simon.davy@thedatalab.org>
2022-05-24 17:35:49 +0100

    fix: record timestamp for flag changes
    Record when a flag was last set or unset. This will be helpful in
    diagnosing issues, as it can help us see when things changed.
    
    This did involve adding a new column to the Flag table, which we've not
    got a mechanism to handle. But errors with the flag table are gracefully
    handled, and its a simple manual ALTER TABLE to run on TPP (the only
    place that currently has the flag table).

--- tests/cli/test_flags.py
+++ tests/cli/test_flags.py
@@ -1,8 +1,16 @@
+import time
+
 import pytest
 
 from jobrunner import queries
 from jobrunner.cli import flags
 from jobrunner.lib import database
+from jobrunner.models import timestamp_to_isoformat
+
+
+# use a fixed time for these tests
+TEST_TIME = time.time()
+TEST_DATESTR = timestamp_to_isoformat(TEST_TIME)
 
 
 def test_get_no_args(capsys, tmp_work_dir):
@@ -11,41 +19,43 @@ def test_get_no_args(capsys, tmp_work_dir):
     assert stdout == ""
     assert stderr == ""
 
-    queries.set_flag("foo", "bar")
+    queries.set_flag("foo", "bar", TEST_TIME)
     flags.run(["get"])
     stdout, stderr = capsys.readouterr()
-    assert stdout == "foo=bar\n"
+    assert stdout == f"foo=bar ({TEST_DATESTR})\n"
     assert stderr == ""
 
 
 def test_args_get(capsys, tmp_work_dir):
     flags.run(["get", "foo"])
     stdout, stderr = capsys.readouterr()
-    assert stdout == "foo=None\n"
+    assert stdout == "foo=None (never set)\n"
     assert stderr == ""
 
-    queries.set_flag("foo", "bar")
+    queries.set_flag("foo", "bar", TEST_TIME)
     flags.run(["get", "foo"])
     stdout, stderr = capsys.readouterr()
-    assert stdout == "foo=bar\n"
+    assert stdout == f"foo=bar ({TEST_DATESTR})\n"
     assert stderr == ""
 
 
-def test_args_set(capsys, tmp_work_dir):
+def test_args_set(capsys, tmp_work_dir, freezer):
+    freezer.move_to(TEST_DATESTR)
     flags.run(["set", "foo=bar"])
     stdout, stderr = capsys.readouterr()
-    assert stdout == "foo=bar\n"
+    assert stdout == f"foo=bar ({TEST_DATESTR})\n"
     assert stderr == ""
-    assert queries.get_flag("foo") == "bar"
+    assert queries.get_flag("foo").value == "bar"
 
 
-def test_args_set_clear(capsys, tmp_work_dir):
+def test_args_set_clear(capsys, tmp_work_dir, freezer):
+    freezer.move_to(TEST_DATESTR)
     queries.set_flag("foo", "bar")
     flags.run(["set", "foo="])
     stdout, stderr = capsys.readouterr()
-    assert stdout == "foo=None\n"
+    assert stdout == f"foo=None ({TEST_DATESTR})\n"
     assert stderr == ""
-    assert queries.get_flag("foo") is None
+    assert queries.get_flag("foo").value is None
 
 
 def test_args_get_create(capsys, tmp_work_dir):
@@ -61,7 +71,8 @@ def test_args_get_create(capsys, tmp_work_dir):
     assert stderr == ""
 
 
-def test_args_set_create(capsys, tmp_work_dir):
+def test_args_set_create(capsys, tmp_work_dir, freezer):
+    freezer.move_to(TEST_DATESTR)
     database.get_connection().execute("DROP TABLE flags")
     with pytest.raises(SystemExit) as e:
         flags.run(["set", "foo=bar"])
@@ -70,6 +81,6 @@ def test_args_set_create(capsys, tmp_work_dir):
 
     flags.run(["set", "foo=bar", "--create"])
     stdout, stderr = capsys.readouterr()
-    assert stdout == "foo=bar\n"
+    assert stdout == f"foo=bar ({TEST_DATESTR})\n"
     assert stderr == ""
-    assert queries.get_flag("foo") == "bar"
+    assert queries.get_flag("foo").value == "bar"
