3551e95bde2c2e6144290f26b8258e7266416147
bloodearnest <simon.davy@thedatalab.org>
2022-06-17 12:20:12 +0100

    fix: ensure paths passed to docker are unix paths
    When jobrunner parses the pipeline config on windows, dummy_data_file is
    a windows path with \ in. When passed verbatim to docker, this blows up.
    So we ensure we convert it to unix path at this point

--- jobrunner/lib/path_utils.py
+++ jobrunner/lib/path_utils.py
@@ -37,3 +37,11 @@ def _iter_dir(directory, match_tree):
         # Otherwise filter this subdirectory using the subtree
         else:
             yield from _iter_dir(item, subtree)
+
+
+def ensure_unix_path(path):
+    "Ensure path is unix path string"
+    # if we're string, handle it as a string
+    if isinstance(path, str):
+        return path.replace("\\", "/")
+    return path.as_posix()
