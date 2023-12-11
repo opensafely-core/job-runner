2fd5236b0d6679a490205f9faf7a01ec7b93f9bc
bloodearnest <simon.davy@thedatalab.org>
2023-12-11 12:42:17 +0000

    Collect and store per-job aggregate metrics
    Stores and updates running totals/aggregates of metrics whilst a job is
    EXECUTING.
    
    To keep executor state separate from scheduler state, we store this
    state in a separate db file altogether. This prevents lock contention on
    the main db file. Unlike the last time we did this, it should not grow
    to exessive size, as we only store 1 small row per job.
    
    For each metric, we calculate and store: lastest sample, cumsum, current
    mean (measured at this point in time), and peak value.
    
    We also add them to the tick trace spans as can be useful.

--- tests/test_record_stats.py
+++ tests/test_record_stats.py
@@ -4,7 +4,7 @@ import time
 from jobrunner import record_stats
 from jobrunner.models import State, StatusCode
 from tests.conftest import get_trace
-from tests.factories import job_factory
+from tests.factories import job_factory, metrics_factory
 
 
 def test_record_tick_trace(db, freezer, monkeypatch):
@@ -28,12 +28,12 @@ def test_record_tick_trace(db, freezer, monkeypatch):
     # this should not be tick'd
     job_factory(state=State.SUCCEEDED, status_code=StatusCode.SUCCEEDED)
 
-    last_run1 = record_stats.record_tick_trace(None)
+    last_run1 = record_stats.record_tick_trace(None, jobs)
     assert len(get_trace("ticks")) == 0
 
     freezer.tick(10)
 
-    last_run2 = record_stats.record_tick_trace(last_run1)
+    last_run2 = record_stats.record_tick_trace(last_run1, jobs)
     assert last_run2 == last_run1 + 10 * 1e9
 
     spans = get_trace("ticks")
@@ -56,7 +56,17 @@ def test_record_tick_trace(db, freezer, monkeypatch):
         if job is running_job:
             assert span.attributes["has_metrics"] is True
             assert span.attributes["cpu_percentage"] == 50.0
+            assert span.attributes["cpu_sample"] == 50.0
+            assert span.attributes["cpu_sample"] == 50.0
+            assert span.attributes["cpu_peak"] == 50.0
+            assert span.attributes["cpu_cumsum"] == 500.0  # 50% * 10s
+            assert span.attributes["cpu_mean"] == 50.0
+
             assert span.attributes["memory_used"] == 1000
+            assert span.attributes["mem_sample"] == 1000
+            assert span.attributes["mem_peak"] == 1000
+            assert span.attributes["mem_cumsum"] == 10000  # 1000 * 10s
+            assert span.attributes["mem_mean"] == 1000
         else:
             assert span.attributes["has_metrics"] is False
 
@@ -64,7 +74,7 @@ def test_record_tick_trace(db, freezer, monkeypatch):
 
 
 def test_record_tick_trace_stats_timeout(db, freezer, monkeypatch):
-    job_factory(status_code=StatusCode.EXECUTING)
+    job = job_factory(status_code=StatusCode.EXECUTING)
 
     def timeout():
         raise subprocess.TimeoutExpired("cmd", 10)
@@ -74,7 +84,7 @@ def test_record_tick_trace_stats_timeout(db, freezer, monkeypatch):
     last_run = time.time()
     freezer.tick(10)
 
-    record_stats.record_tick_trace(last_run)
+    record_stats.record_tick_trace(last_run, [job])
     assert len(get_trace("ticks")) == 2
 
     spans = get_trace("ticks")
@@ -82,13 +92,14 @@ def test_record_tick_trace_stats_timeout(db, freezer, monkeypatch):
 
     assert "cpu_percentage" not in span.attributes
     assert "memory_used" not in span.attributes
+    assert "mem_peak" not in span.attributes
     assert span.attributes["has_metrics"] is False
     assert span.attributes["stats_timeout"] is True
     assert span.attributes["stats_error"] is False
 
 
 def test_record_tick_trace_stats_error(db, freezer, monkeypatch):
-    job_factory(status_code=StatusCode.EXECUTING)
+    job = job_factory(status_code=StatusCode.EXECUTING)
 
     def error():
         raise subprocess.CalledProcessError(
@@ -98,7 +109,7 @@ def test_record_tick_trace_stats_error(db, freezer, monkeypatch):
     monkeypatch.setattr(record_stats, "get_job_stats", error)
 
     last_run = time.time()
-    record_stats.record_tick_trace(last_run)
+    record_stats.record_tick_trace(last_run, [job])
     assert len(get_trace("ticks")) == 2
 
     spans = get_trace("ticks")
@@ -106,6 +117,7 @@ def test_record_tick_trace_stats_error(db, freezer, monkeypatch):
 
     assert "cpu_percentage" not in span.attributes
     assert "memory_used" not in span.attributes
+    assert "mem_peak" not in span.attributes
     assert span.attributes["has_metrics"] is False
     assert span.attributes["stats_timeout"] is False
     assert span.attributes["stats_error"] is True
@@ -117,3 +129,73 @@ def test_record_tick_trace_stats_error(db, freezer, monkeypatch):
     assert root.events[0].attributes["cmd"] == "test cmd"
     assert root.events[0].attributes["output"] == "stderr\n\nstdout"
     assert root.events[0].name == "stats_error"
+
+
+def test_update_job_metrics(db):
+
+    job = job_factory(status_code=StatusCode.EXECUTING)
+    metrics_factory(job)
+
+    metrics = record_stats.read_job_metrics(job.id)
+
+    assert metrics == {}
+
+    # 50%/100m for 1s
+    record_stats.update_job_metrics(
+        job,
+        {"cpu_percentage": 50, "memory_used": 100},
+        duration_s=1.0,
+        runtime_s=1.0,
+    )
+
+    metrics = record_stats.read_job_metrics(job.id)
+    assert metrics == {
+        "cpu_cumsum": 50.0,
+        "cpu_mean": 50.0,
+        "cpu_peak": 50,
+        "cpu_sample": 50,
+        "mem_cumsum": 100.0,
+        "mem_mean": 100.0,
+        "mem_peak": 100,
+        "mem_sample": 100,
+    }
+
+    # 100%/1000m for 1s
+    record_stats.update_job_metrics(
+        job,
+        {"cpu_percentage": 100, "memory_used": 1000},
+        duration_s=1.0,
+        runtime_s=2.0,
+    )
+
+    metrics = record_stats.read_job_metrics(job.id)
+    assert metrics == {
+        "cpu_cumsum": 150.0,
+        "cpu_mean": 75.0,
+        "cpu_peak": 100,
+        "cpu_sample": 100,
+        "mem_cumsum": 1100.0,
+        "mem_mean": 550.0,
+        "mem_peak": 1000,
+        "mem_sample": 1000,
+    }
+
+    # 100%/1000m for 8s
+    record_stats.update_job_metrics(
+        job,
+        {"cpu_percentage": 100, "memory_used": 1000},
+        duration_s=8.0,
+        runtime_s=10.0,
+    )
+
+    metrics = record_stats.read_job_metrics(job.id)
+    assert metrics == {
+        "cpu_cumsum": 950.0,
+        "cpu_mean": 95.0,
+        "cpu_peak": 100,
+        "cpu_sample": 100,
+        "mem_cumsum": 9100.0,
+        "mem_mean": 910.0,
+        "mem_peak": 1000,
+        "mem_sample": 1000,
+    }
