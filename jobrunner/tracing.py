157fae30c45bc608bef6f8b70f933809dccea9fd
Simon Davy <simon.davy@thedatalab.org>
2024-03-21 12:42:57 +0000

    Update jobrunner/tracing.py
    Co-authored-by: Dave Evans <dave.evans@thedatalab.org>

--- jobrunner/tracing.py
+++ jobrunner/tracing.py
@@ -52,7 +52,7 @@ def setup_default_tracing(set_global=True):
     if "OTEL_EXPORTER_OTLP_HEADERS" in os.environ:
         # workaround for env file parsing issues
         cleaned_headers = os.environ["OTEL_EXPORTER_OTLP_HEADERS"].strip("\"'")
-        # put back into env to be parsed properlh
+        # put back into env to be parsed properly
         os.environ["OTEL_EXPORTER_OTLP_HEADERS"] = cleaned_headers
 
         if "OTEL_EXPORTER_OTLP_ENDPOINT" not in os.environ:
