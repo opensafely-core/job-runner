7f5539e00418a4c3cbaf3070cb0a2d923a7510e3
bloodearnest <simon.davy@thedatalab.org>
2025-04-02 21:26:11 +0100

    Add an initial implementation of the Task API
    This anticipates the agent/controller split, so adds the respective APIs
    into new `agent` and `controller` directories.
    
    The controller task API provides a way to insert new tasks, and to mark
    them as completed, i.e. no longer active.
    
    The agent task API provides functions to get a list of current tasks,
    and to update a task's stage and data.  Right now, the agent task API
    writes to the controllers tasks table directly - this is a temporary
    hack to allow us to separate the agent and controller out. It will later
    be replaced with a HTTP API that the controller will provide and the
    agent will call.
    
    Whilst a task is executing, the main purpose of `update_controller` is
    to provide fine grained task stage transitions with timing to the
    controller to be used for telemetry.  Once the task is completed, the
    `update_controller` call should include the results of the task as
    a json dict.  That dict is currently unspecified, as that is part of the
    split work.
    
    We currently only define one task, RUNJOB.
    
    This is a fairly rough cut API, and we may well want to rework it, but
    it should hopefully help us to do the split work faster.

---
+++ jobrunner/agent/task_api.py
@@ -0,0 +1,44 @@
+from jobrunner.lib import database  # cheating!
+from jobrunner.models import Task as ControllerTask  # cheating!
+from jobrunner.schema import AgentTask, TaskStage
+
+
+def get_active_tasks() -> list[AgentTask]:
+    """Get a list of active tasks from the controller"""
+    # cheating for now - should be HTTP API call
+    return [
+        AgentTask(
+            id=t.id,
+            type=t.type,
+            definition=t.definition,
+            created_at=t.created_at,
+        )
+        for t in database.find_where(ControllerTask, active=True)
+    ]
+
+
+def update_controller(
+    task: AgentTask,
+    stage: TaskStage,
+    timestamp: int,
+    results: dict = None,
+):
+    """Update the controller with the current state of the task.
+
+    stage: the current stage of the task from the agent's perspective
+    timestamp: ns timestamp of current stage was acheived (for accurate telemetry)
+    results: optional dictionary of completed results of this task, expected to be immutable
+    """
+    # cheating for now - should be HTTP API call, which ends up updating the
+    # controller db, but we just update the db directly
+    #
+    # this currently just updates the task table, and lets the main controller
+    # loop update the jobs table as needed, and handle a completed task. In the
+    # future, we may want the HTTP handler to do both, so that the main loop
+    # does not need to handle agent updates and completed jobs at all.  But all
+    # we have currently is the loop, so we'll do that logic there for step 1
+    db_task = database.find_one(ControllerTask, id=task.id)
+    db_task.agent_stage = stage
+    db_task.agent_stage_ns = timestamp
+    db_task.agent_results = results
+    database.update(db_task)
