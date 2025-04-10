a41bd82039eb5a7032bddb1c2763b7e35c3a88b0
David Evans <dave.evans@thedatalab.org>
2025-04-10 15:33:45 +0100

    Convert next test

--- tests/controller/test_main.py
+++ tests/controller/test_main.py
@@ -82,22 +82,17 @@ def test_handle_job_pending_dependency_failed(db):
 
 
 def test_handle_pending_job_waiting_on_dependency(db):
-    api = StubExecutorAPI()
-    dependency = api.add_test_job(ExecutorState.EXECUTING, State.RUNNING)
-
-    job = api.add_test_job(
-        ExecutorState.UNKNOWN,
-        State.PENDING,
+    dependency = job_factory()
+    job = job_factory(
+        state=State.PENDING,
         job_request_id=dependency.job_request_id,
         action="action2",
         wait_for_job_ids=[dependency.id],
     )
 
-    run.handle_job(job, api)
+    run_controller_loop_once()
 
-    # executor state
-    assert job.id not in api.tracker["prepare"]
-    assert api.get_status(job).state == ExecutorState.UNKNOWN
+    job = database.find_one(Job, id=job.id)
 
     # our state
     assert job.state == State.PENDING
