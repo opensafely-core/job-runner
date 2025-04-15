6b01b808dc4e826cbbab8bb53ef9eee8de2a4e9f
bloodearnest <simon.davy@thedatalab.org>
2025-04-15 18:22:29 +0100

    Add missing coverage from inject_secrets
    Fix typo

--- tests/agent/test_main.py
+++ tests/agent/test_main.py
@@ -4,22 +4,22 @@ from tests.factories import job_definition_factory
 
 
 def test_inject_db_secrets_dummy_db(monkeypatch, db):
-    defintion = job_definition_factory()
-    defintion.allow_database_access = True
-    defintion.database_name = "FULL"
+    definition = job_definition_factory()
+    definition.allow_database_access = True
+    definition.database_name = "FULL"
 
     monkeypatch.setattr(config, "USING_DUMMY_DATA_BACKEND", True)
     monkeypatch.setattr(config, "DATABASE_URLS", {"FULL": "dburl"})
 
-    main.inject_db_secrets(defintion)
+    main.inject_db_secrets(definition)
 
-    assert "DATABASE_URL" not in defintion.env
+    assert "DATABASE_URL" not in definition.env
 
 
 def test_inject_db_secrets(monkeypatch, db):
-    defintion = job_definition_factory()
-    defintion.allow_database_access = True
-    defintion.database_name = "FULL"
+    definition = job_definition_factory()
+    definition.allow_database_access = True
+    definition.database_name = "FULL"
 
     monkeypatch.setattr(config, "USING_DUMMY_DATA_BACKEND", False)
     monkeypatch.setattr(config, "DATABASE_URLS", {"FULL": "dburl"})
@@ -28,10 +28,31 @@ def test_inject_db_secrets(monkeypatch, db):
     monkeypatch.setattr(config, "PRESTO_TLS_CERT", "cert")
     monkeypatch.setattr(config, "EMIS_ORGANISATION_HASH", "hash")
 
-    main.inject_db_secrets(defintion)
+    main.inject_db_secrets(definition)
 
-    assert defintion.env["DATABASE_URL"] == "dburl"
-    assert defintion.env["TEMP_DATABASE_NAME"] == "tempdb"
-    assert defintion.env["PRESTO_TLS_KEY"] == "key"
-    assert defintion.env["PRESTO_TLS_CERT"] == "cert"
-    assert defintion.env["EMIS_ORGANISATION_HASH"] == "hash"
+    assert definition.env["DATABASE_URL"] == "dburl"
+    assert definition.env["TEMP_DATABASE_NAME"] == "tempdb"
+    assert definition.env["PRESTO_TLS_KEY"] == "key"
+    assert definition.env["PRESTO_TLS_CERT"] == "cert"
+    assert definition.env["EMIS_ORGANISATION_HASH"] == "hash"
+
+
+def test_inject_db_secrets_none_configured(monkeypatch, db):
+    definition = job_definition_factory()
+    definition.allow_database_access = True
+    definition.database_name = "FULL"
+
+    monkeypatch.setattr(config, "USING_DUMMY_DATA_BACKEND", False)
+    monkeypatch.setattr(config, "DATABASE_URLS", {"FULL": "dburl"})
+    monkeypatch.setattr(config, "TEMP_DATABASE_NAME", None)
+    monkeypatch.setattr(config, "PRESTO_TLS_KEY", None)
+    monkeypatch.setattr(config, "PRESTO_TLS_CERT", None)
+    monkeypatch.setattr(config, "EMIS_ORGANISATION_HASH", None)
+
+    main.inject_db_secrets(definition)
+
+    assert definition.env["DATABASE_URL"] == "dburl"
+    assert "TEMP_DATABASE_NAME" not in definition.env
+    assert "PRESTO_TLS_KEY" not in definition.env
+    assert "PRESTO_TLS_CERT" not in definition.env
+    assert "EMIS_ORGANISATION_HASH" not in definition.env
