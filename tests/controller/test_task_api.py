c7874390bec77aeb563b4265d17647e2acbcf1b2
Becky Smith <becky.smith@thedatalab.org>
2025-04-16 15:51:36 +0100

    Update imports

--- tests/controller/test_task_api.py
+++ tests/controller/test_task_api.py
@@ -1,7 +1,7 @@
 from jobrunner.controller import task_api
+from jobrunner.controller.main import job_to_job_definition
 from jobrunner.job_executor import JobDefinition
 from jobrunner.models import Task, TaskType
-from jobrunner.run import job_to_job_definition
 from tests.factories import job_factory
 
 
