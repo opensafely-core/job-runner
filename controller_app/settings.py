de252e13a4563510a25eb1497517f17275ab3c03
Tom Ward <tomward@fmail.co.uk>
2025-04-17 13:43:03 +0100

    Change the default DEBUG setting to False
    * this is being used in production!

--- controller_app/settings.py
+++ controller_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7x$$d9^(-rb2uj57*d%u#z68fh*@y8s)8zu(*_t-ck%5jmk)ef"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["controller.opensafely.org"]
 
