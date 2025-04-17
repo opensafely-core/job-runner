edb38d1464aa7c8bb8d5db4a066f08ec8467971f
Becky Smith <becky.smith@thedatalab.org>
2025-04-17 12:53:08 +0100

    Remove the LoggingExecutor and associated tests

--- jobrunner/executors/__init__.py
+++ jobrunner/executors/__init__.py
@@ -1,10 +1,9 @@
 import importlib
 
 from jobrunner import config
-from jobrunner.executors.logging import LoggingExecutor
 
 
 def get_executor_api():
     module_name, cls = config.EXECUTOR.split(":", 1)
     module = importlib.import_module(module_name)
-    return LoggingExecutor(getattr(module, cls)())
+    return getattr(module, cls)()
