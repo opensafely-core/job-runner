7b41434189d37a2e797c9df363a616168c8a7180
Becky Smith <becky.smith@thedatalab.org>
2025-05-07 17:12:06 +0100

    Temporary wrapper cli modules
    The backends will need to be updated to call the new
    agent/controller-specific commands. Until they are, these will
    ensure that the current just commands on backends continue to work
    as expected.

--- jobrunner/cli/migrate.py
+++ jobrunner/cli/migrate.py
@@ -1,27 +1,7 @@
-"""
-Run any pending database migrations
-"""
-
-import argparse
 import sys
-from pathlib import Path
 
-from jobrunner.config import controller as config
-from jobrunner.lib import database, log_utils
-
-
-def run(argv):
-    log_utils.configure_logging()
-    parser = argparse.ArgumentParser(description=__doc__.partition("\n\n")[0])
-    parser.add_argument(
-        "--dbpath",
-        type=Path,
-        default=config.DATABASE_FILE,
-        help="db file to migrate (defaults to configured db)",
-    )
-    args = parser.parse_args(argv)
-    database.ensure_db(args.dbpath, verbose=True)
+from jobrunner.cli.controller import migrate
 
 
 if __name__ == "__main__":
-    run(sys.argv[1:])
+    migrate.run(sys.argv[1:])
