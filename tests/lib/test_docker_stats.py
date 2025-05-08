d34ef2a8d2173883519a427e8046e8151c0c6332
bloodearnest <simon.davy@thedatalab.org>
2025-05-08 17:33:53 +0100

    consolidate pointless wrapper function

--- tests/lib/test_docker_stats.py
+++ tests/lib/test_docker_stats.py
@@ -4,18 +4,18 @@ from jobrunner.lib import docker, docker_stats
 
 
 @pytest.mark.needs_docker
-def test_get_container_stats(docker_cleanup):
+def test_get_job_stats(docker_cleanup):
     docker.run("os-job-test", [docker.MANAGEMENT_CONTAINER_IMAGE, "sleep", "10"])
-    containers = docker_stats.get_container_stats()
+    containers = docker_stats.get_job_stats()
     assert isinstance(containers["test"]["cpu_percentage"], float)
     assert isinstance(containers["test"]["memory_used"], int)
 
 
 @pytest.mark.needs_docker
-def test_get_container_stats_regression(docker_cleanup):
+def test_get_job_stats_regression(docker_cleanup):
     # we had a bug where we use str.lstrip("os-job-") :facepalm"
     # id starts with o
     docker.run("os-job-otest", [docker.MANAGEMENT_CONTAINER_IMAGE, "sleep", "10"])
-    containers = docker_stats.get_container_stats()
+    containers = docker_stats.get_job_stats()
     assert isinstance(containers["otest"]["cpu_percentage"], float)
     assert isinstance(containers["otest"]["memory_used"], int)
