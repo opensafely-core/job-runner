11fc080854361882eec0ef27bd8ba6fc99cf2d35
Peter Inglesby <peter.inglesby@gmail.com>
2025-05-09 12:02:38 +0100

    Remove obsolete TODO

--- jobrunner/agent/main.py
+++ jobrunner/agent/main.py
@@ -182,9 +182,6 @@ def handle_run_job_task(task, api):
     """
     job = JobDefinition.from_dict(task.definition)
     with set_log_context(job_definition=job):
-        # TODO: if job.allow_database_access, then we need to populate job.env with
-        # various secrets, as per run.py:job_to_job_definition
-
         job_status = api.get_status(job)
 
         span = trace.get_current_span()
