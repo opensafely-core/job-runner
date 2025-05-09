8437189a09a5095410c9c4172d57909316e654fd
Providence-o <97491889+Providence-o@users.noreply.github.com>
2025-05-09 16:31:35 +0100

    API endpoint for updating tasks

--- controller_app/urls.py
+++ controller_app/urls.py
@@ -6,4 +6,5 @@ from controller_app import views
 urlpatterns = [
     path("", views.index),
     path("<str:backend>/tasks/", views.active_tasks, name="active_tasks"),
+    path("<str:backend>/task/update/", views.update_task, name="update_task"),
 ]
