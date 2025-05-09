138b452dd495c410fd0c56df28f734b5ef6422f8
Becky Smith <becky.smith@thedatalab.org>
2025-05-09 11:34:24 +0100

    API endpoint for active tasks
    This encodes the backend in the URL scheme. This may want to change
    later if/when we use an authentication token to identify the backend.

--- controller_app/views.py
+++ controller_app/views.py
@@ -1,8 +1,15 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from jobrunner.controller.task_api import get_active_tasks
+
 
 @csrf_exempt
 def index(request):
     response = JsonResponse({"method": request.method})
     return response
+
+
+def active_tasks(request, backend):
+    active_tasks = [task.asdict() for task in get_active_tasks(backend)]
+    return JsonResponse({"tasks": active_tasks})
