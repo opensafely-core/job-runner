8346b93d5d52954599c5fe04fbf74ab618004f89
Providence-o <97491889+Providence-o@users.noreply.github.com>
2025-05-09 16:31:35 +0100

    API endpoint for updating tasks

--- controller_app/views.py
+++ controller_app/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
-from jobrunner.controller.task_api import get_active_tasks
+from jobrunner.controller.task_api import get_active_tasks, handle_task_update
 
 
 @csrf_exempt
@@ -13,3 +14,19 @@ def index(request):
 def active_tasks(request, backend):
     active_tasks = [task.asdict() for task in get_active_tasks(backend)]
     return JsonResponse({"tasks": active_tasks})
+
+
+@require_POST
+def update_task(request, backend):
+    update_task_info = request.POST
+
+    task_id = update_task_info["task_id"]
+    stage = update_task_info["stage"]
+    results = update_task_info["results"]
+    complete = update_task_info["complete"]
+
+    handle_task_update(task_id=task_id, stage=stage, results=results, complete=complete)
+
+    response = JsonResponse({"status": "Task updated successfully"})
+
+    return response
