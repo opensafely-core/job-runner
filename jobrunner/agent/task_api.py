030e990f95c36478cc043415d30c18d7319451d5
Becky Smith <becky.smith@thedatalab.org>
2025-05-12 16:41:44 +0100

    Helper class for calling the tasks API

--- jobrunner/agent/task_api.py
+++ jobrunner/agent/task_api.py
@@ -1,5 +1,34 @@
+import logging
+from urllib.parse import urljoin
+
+import requests
+
+from jobrunner.config import agent as config
 from jobrunner.controller import task_api as controller_task_api  # cheating!
 from jobrunner.schema import AgentTask
+
+
+log = logging.getLogger(__name__)
+
+
+class TaskApi:
+    def __init__(self):
+        self.base_url = urljoin(config.TASK_API_ENDPOINT, config.BACKEND)
+        self.session = requests.Session()
+
+    def get_json(self, path):
+        return self.request_json("GET", path)
+
+    def request_json(self, method, path, data=None):
+        data = data or {}
+        url = f"{self.base_url}/{path}"
+        response = self.session.request(method, url, data=data)
+        try:
+            response.raise_for_status()
+        except Exception as e:
+            log.exception(e)
+            raise
+        return response.json()
 
 
 def get_active_tasks(backend: str) -> list[AgentTask]:
