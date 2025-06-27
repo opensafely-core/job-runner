b93831ada14fa0d47ced36e4fe2997eba2f82575
Tom Ward <tomward@fmail.co.uk>
2025-06-27 14:16:55 +0100

    Remove cohortextractor action special-case

--- jobrunner/actions.py
+++ jobrunner/actions.py
@@ -4,7 +4,6 @@ import shlex
 
 from pipeline.exceptions import ProjectValidationError
 from pipeline.models import Action
-from pipeline.outputs import get_output_dirs
 
 
 class UnknownActionError(ProjectValidationError):
@@ -52,15 +51,6 @@ def get_action_specification(config, action_id):
             json.dumps(action_spec.config).replace("'", r"\u0027"),
         ]
 
-    # Special case handling for the `cohortextractor generate_cohort` command
-    if is_cohortextractor_generate_cohort(run_parts):
-        output_dirs = get_output_dirs(action_spec.outputs)
-
-        if len(output_dirs) == 1:  # pragma: no branch
-            # Automatically configure the cohortextractor to produce output in the
-            # directory the `outputs` spec is expecting.
-            run_parts.append(f"--output-dir={output_dirs[0]}")
-
     run_command = shlex.join(run_parts)
 
     return ActionSpecification(
@@ -68,12 +58,4 @@ def get_action_specification(config, action_id):
         needs=action_spec.needs,
         outputs=action_spec.outputs.dict(),
         action=action_spec,
-    )
-
-
-def is_cohortextractor_generate_cohort(args):
-    return (
-        len(args) > 1
-        and args[0].startswith("cohortextractor:")
-        and args[1] == "generate_cohort"
     )
