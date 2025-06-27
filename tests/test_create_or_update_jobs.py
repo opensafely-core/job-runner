a8c8e0daf8ea0ecaeb52a00d553d726159bc095a
Tom Ward <tomward@fmail.co.uk>
2025-06-27 14:03:48 +0100

    Update tests to remove cohortextractor
    * uses updated git-repo fixture
    * nb. this didn't actually /use/ cohortextractor, it just used the name

--- tests/test_create_or_update_jobs.py
+++ tests/test_create_or_update_jobs.py
@@ -33,9 +33,9 @@ def test_create_or_update_jobs(tmp_work_dir, db):
         id="123",
         repo_url=repo_url,
         # GIT_DIR=tests/fixtures/git-repo git rev-parse v1
-        commit="cfbd0fe545d4e4c0747f0746adaa79ce5f8dfc74",
+        commit="d090466f63b0d68084144d8f105f0d6e79a0819e",
         branch="v1",
-        requested_actions=["generate_cohort"],
+        requested_actions=["generate_dataset"],
         cancelled_actions=[],
         workspace="1",
         codelists_ok=True,
@@ -52,15 +52,17 @@ def test_create_or_update_jobs(tmp_work_dir, db):
     assert old_job.job_request_id == "123"
     assert old_job.state == State.PENDING
     assert old_job.repo_url == repo_url
-    assert old_job.commit == "cfbd0fe545d4e4c0747f0746adaa79ce5f8dfc74"
+    assert old_job.commit == "d090466f63b0d68084144d8f105f0d6e79a0819e"
     assert old_job.workspace == "1"
-    assert old_job.action == "generate_cohort"
+    assert old_job.action == "generate_dataset"
     assert old_job.wait_for_job_ids == []
     assert old_job.requires_outputs_from == []
     assert old_job.run_command == (
-        "cohortextractor:latest generate_cohort --output-dir=."
+        "ehrql:v1 generate-dataset analysis/dataset_definition.py --output output/dataset.csv.gz"
     )
-    assert old_job.output_spec == {"highly_sensitive": {"cohort": "input.csv"}}
+    assert old_job.output_spec == {
+        "highly_sensitive": {"dataset": "output/dataset.csv.gz"}
+    }
     assert old_job.backend == "test"
     assert old_job.status_message == "Created"
     # Check no new jobs created from same JobRequest
@@ -78,7 +80,7 @@ def test_create_or_update_jobs_with_git_error(tmp_work_dir):
         repo_url=repo_url,
         commit=bad_commit,
         branch="v1",
-        requested_actions=["generate_cohort"],
+        requested_actions=["generate_dataset"],
         cancelled_actions=[],
         workspace="1",
         codelists_ok=True,
@@ -117,7 +119,7 @@ def test_create_or_update_jobs_with_unhandled_error(tmp_work_dir, db):
         # GIT_DIR=tests/fixtures/git-repo git rev-parse v1
         commit="cfbd0fe545d4e4c0747f0746adaa79ce5f8dfc74",
         branch="v1",
-        requested_actions=["generate_cohort"],
+        requested_actions=["generate_dataset"],
         cancelled_actions=[],
         workspace="1",
         codelists_ok=True,
@@ -147,22 +149,22 @@ def test_create_or_update_jobs_with_unhandled_error(tmp_work_dir, db):
 TEST_PROJECT = """
 version: '1.0'
 actions:
-  generate_cohort:
-    run: cohortextractor:latest generate_cohort
+  generate_dataset:
+    run: ehrql:v1 generate-dataset analysis/dataset_definition.py --output output.csv.gz
     outputs:
       highly_sensitive:
-        cohort: input.csv
+        cohort: output.csv.gz
 
   prepare_data_1:
     run: stata-mp:latest analysis/prepare_data_1.do
-    needs: [generate_cohort]
+    needs: [generate_dataset]
     outputs:
       highly_sensitive:
         data: prepared_1.dta
 
   prepare_data_2:
     run: stata-mp:latest analysis/prepare_data_2.do
-    needs: [generate_cohort]
+    needs: [generate_dataset]
     outputs:
       highly_sensitive:
         data: prepared_2.dta
@@ -181,7 +183,7 @@ def test_adding_job_creates_dependencies(tmp_work_dir):
     analyse_job = find_one(Job, action="analyse_data")
     prepare_1_job = find_one(Job, action="prepare_data_1")
     prepare_2_job = find_one(Job, action="prepare_data_2")
-    generate_job = find_one(Job, action="generate_cohort")
+    generate_job = find_one(Job, action="generate_dataset")
     assert set(analyse_job.wait_for_job_ids) == {prepare_1_job.id, prepare_2_job.id}
     assert prepare_1_job.wait_for_job_ids == [generate_job.id]
     assert prepare_2_job.wait_for_job_ids == [generate_job.id]
@@ -193,7 +195,7 @@ def test_existing_active_jobs_are_picked_up_when_checking_dependencies(tmp_work_
         make_job_request(action="prepare_data_1"), TEST_PROJECT
     )
     prepare_1_job = find_one(Job, action="prepare_data_1")
-    generate_job = find_one(Job, action="generate_cohort")
+    generate_job = find_one(Job, action="generate_dataset")
     assert prepare_1_job.wait_for_job_ids == [generate_job.id]
     # Now schedule a job which has the above jobs as dependencies
     create_jobs_with_project_file(make_job_request(action="analyse_data"), TEST_PROJECT)
@@ -218,7 +220,7 @@ def test_existing_active_jobs_for_other_backends_are_ignored_when_checking_depen
 
     # There are now 2 of each job
     for action in [
-        "generate_cohort",
+        "generate_dataset",
         "prepare_data_1",
         "prepare_data_2",
         "analyse_data",
@@ -227,12 +229,12 @@ def test_existing_active_jobs_for_other_backends_are_ignored_when_checking_depen
 
     # Check that they're waiting on the right existing jobs
     for backend in ["foo", "bar"]:
-        generate_cohort_job = find_one(Job, action="generate_cohort", backend=backend)
+        generate_dataset_job = find_one(Job, action="generate_dataset", backend=backend)
         prepare_1_job = find_one(Job, action="prepare_data_1", backend=backend)
         prepare_2_job = find_one(Job, action="prepare_data_2", backend=backend)
         analyse_job = find_one(Job, action="analyse_data", backend=backend)
 
-        assert set(prepare_1_job.wait_for_job_ids) == {generate_cohort_job.id}
+        assert set(prepare_1_job.wait_for_job_ids) == {generate_dataset_job.id}
         assert set(analyse_job.wait_for_job_ids) == {prepare_1_job.id, prepare_2_job.id}
 
 
@@ -241,7 +243,7 @@ def test_existing_succeeded_jobs_are_picked_up_when_checking_dependencies(tmp_wo
         make_job_request(action="prepare_data_1"), TEST_PROJECT
     )
     prepare_1_job = find_one(Job, action="prepare_data_1")
-    generate_job = find_one(Job, action="generate_cohort")
+    generate_job = find_one(Job, action="generate_dataset")
     assert prepare_1_job.wait_for_job_ids == [generate_job.id]
     # make the generate_job succeeded
     update_where(Job, {"state": State.SUCCEEDED}, id=generate_job.id)
@@ -260,9 +262,9 @@ def test_existing_cancelled_jobs_are_ignored_up_when_checking_dependencies(
     tmp_work_dir,
 ):
     create_jobs_with_project_file(
-        make_job_request(action="generate_cohort"), TEST_PROJECT
+        make_job_request(action="generate_dataset"), TEST_PROJECT
     )
-    cancelled_generate_job = find_one(Job, action="generate_cohort")
+    cancelled_generate_job = find_one(Job, action="generate_dataset")
     update_where(Job, {"cancelled": True}, id=cancelled_generate_job.id)
 
     # Now schedule a job which has the above job as a dependency
@@ -272,7 +274,7 @@ def test_existing_cancelled_jobs_are_ignored_up_when_checking_dependencies(
 
     # Check that it's spawned a new instance of the cancelled job and wired up the dependencies correctly
     prepare_job = find_one(Job, action="prepare_data_1")
-    new_generate_job = find_one(Job, action="generate_cohort", cancelled=0)
+    new_generate_job = find_one(Job, action="generate_dataset", cancelled=0)
     assert new_generate_job.id != cancelled_generate_job.id
 
     assert len(prepare_job.wait_for_job_ids) == 1
@@ -295,7 +297,7 @@ def test_run_all_ignores_failed_actions_that_have_been_removed(tmp_work_dir):
 
     # Since then all the healthy, vigorous actions have been successfully run individually
     request = make_job_request(
-        actions=["generate_cohort", "prepare_data_1", "prepare_data_2", "analyse_data"]
+        actions=["generate_dataset", "prepare_data_1", "prepare_data_2", "analyse_data"]
     )
     create_jobs_with_project_file(request, TEST_PROJECT)
     update_where(Job, {"state": State.SUCCEEDED}, job_request_id=request.id)
@@ -313,7 +315,7 @@ def test_cancelled_jobs_are_flagged(tmp_work_dir):
     analyse_job = find_one(Job, action="analyse_data")
     prepare_1_job = find_one(Job, action="prepare_data_1")
     prepare_2_job = find_one(Job, action="prepare_data_2")
-    generate_job = find_one(Job, action="generate_cohort")
+    generate_job = find_one(Job, action="generate_dataset")
     assert analyse_job.cancelled == 0
     assert prepare_1_job.cancelled == 1
     assert prepare_2_job.cancelled == 1
@@ -360,7 +362,7 @@ def test_validate_job_request(patch_config, params, exc_msg, exc_cls, monkeypatc
         # GIT_DIR=tests/fixtures/git-repo git rev-parse v1
         commit="d1e88b31cbe8f67c58f938adb5ee500d54a69764",
         branch="v1",
-        requested_actions=["generate_cohort"],
+        requested_actions=["generate_dataset"],
         cancelled_actions=[],
         workspace="1",
         codelists_ok=True,
@@ -385,7 +387,7 @@ def make_job_request(action=None, actions=None, **kwargs):
         if action:
             actions = [action]
         else:
-            actions = ["generate_cohort"]
+            actions = ["generate_dataset"]
     job_request = JobRequest(
         id=str(uuid.uuid4()),
         repo_url="https://example.com/repo.git",
@@ -421,13 +423,13 @@ def test_create_jobs_already_succeeded_is_rerun(db, tmp_work_dir):
         make_job_request(action="prepare_data_1"), TEST_PROJECT
     )
     prepare_1_job = find_one(Job, action="prepare_data_1")
-    generate_job = find_one(Job, action="generate_cohort")
+    generate_job = find_one(Job, action="generate_dataset")
     update_where(Job, {"state": State.SUCCEEDED}, id=generate_job.id)
     update_where(Job, {"state": State.SUCCEEDED}, id=prepare_1_job.id)
     create_jobs_with_project_file(
         make_job_request(action="prepare_data_1"), TEST_PROJECT
     )
-    generate_jobs = find_where(Job, action="generate_cohort")
+    generate_jobs = find_where(Job, action="generate_dataset")
     assert len(generate_jobs) == 1
     prepare_1_jobs = find_where(Job, action="prepare_data_1")
     assert len(prepare_1_jobs) == 2
@@ -438,14 +440,14 @@ def test_create_jobs_force_run_dependencies(db, tmp_work_dir):
         make_job_request(action="prepare_data_1"), TEST_PROJECT
     )
     prepare_1_job = find_one(Job, action="prepare_data_1")
-    generate_job = find_one(Job, action="generate_cohort")
+    generate_job = find_one(Job, action="generate_dataset")
     update_where(Job, {"state": State.SUCCEEDED}, id=generate_job.id)
     update_where(Job, {"state": State.SUCCEEDED}, id=prepare_1_job.id)
     create_jobs_with_project_file(
         make_job_request(action="prepare_data_1", force_run_dependencies=True),
         TEST_PROJECT,
     )
-    generate_jobs = find_where(Job, action="generate_cohort")
+    generate_jobs = find_where(Job, action="generate_dataset")
     assert len(generate_jobs) == 2
     prepare_1_jobs = find_where(Job, action="prepare_data_1")
     assert len(prepare_1_jobs) == 2
@@ -456,13 +458,13 @@ def test_create_jobs_reruns_failed_dependencies(db, tmp_work_dir):
         make_job_request(action="prepare_data_1"), TEST_PROJECT
     )
     prepare_1_job = find_one(Job, action="prepare_data_1")
-    generate_job = find_one(Job, action="generate_cohort")
+    generate_job = find_one(Job, action="generate_dataset")
     update_where(Job, {"state": State.FAILED}, id=generate_job.id)
     update_where(Job, {"state": State.SUCCEEDED}, id=prepare_1_job.id)
     create_jobs_with_project_file(
         make_job_request(action="prepare_data_1"), TEST_PROJECT
     )
-    generate_jobs = find_where(Job, action="generate_cohort")
+    generate_jobs = find_where(Job, action="generate_dataset")
     assert len(generate_jobs) == 2
     prepare_1_jobs = find_where(Job, action="prepare_data_1")
     assert len(prepare_1_jobs) == 2
@@ -538,7 +540,7 @@ def test_create_job_from_exception_stale_codelist(db):
 
 @pytest.mark.parametrize(
     "requested_action,expect_error",
-    [("generate_cohort", True), ("analyse_data", True), ("standalone_action", False)],
+    [("generate_dataset", True), ("analyse_data", True), ("standalone_action", False)],
 )
 def test_create_or_update_jobs_with_out_of_date_codelists(
     tmp_work_dir, requested_action, expect_error
@@ -559,7 +561,7 @@ def test_create_or_update_jobs_with_out_of_date_codelists(
         with pytest.raises(
             StaleCodelistError,
             match=re.escape(
-                "Codelists are out of date (required by action generate_cohort)"
+                "Codelists are out of date (required by action generate_dataset)"
             ),
         ):
             create_jobs_with_project_file(job_request, project)
