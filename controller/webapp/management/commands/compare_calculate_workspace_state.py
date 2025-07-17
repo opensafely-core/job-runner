f55b2b6af4f01aaa2c0623225a56f932d16d2dcb
Mike Kelly <mike.kelly@thedatalab.org>
2025-07-17 08:15:27 +0100

    Reimplement `calculate_workspace_state` with queryset.
    This is a proof-of-concept commit and PR.
    
    In a management command, reimplement `calculate_workspace_state` using
    querysets and a partition approach and compare perfomance with the extant
    implementation that uses our homebrew ORM and some manipulation in Python.
    
    Very brief initial profiling shows this as approximately 50 times faster on my
    machine when tested against around 2000 jobs/action from the
    https://github.com/opensafely/disparities-comparison repo.
    
    This may demonstrate that this function can be optimized sufficiently to use
    synchronously, and that using the Django ORM is a viable alternative to
    optimizing the homebrew ORM code used. The Django ORM approach can be used
    alonside code using the homebrew approach, which could enable a gradual
    switchover.
    
    Clearly the homebrew ORM code could be optimized further (eg it does some work
    in Python that could be off-loaded to the database engine with `FIRST_VALUE`
    and `PARTITION`, and I think we know the value-type unpacking is slow). Maybe
    there's a better queryset approach.
    
    My very brief assessment is that the queryset code needs at most three passes
    through the rows, O(n), where the extant approach uses a sub-group and sorting
    approach that may be O(n log n) or possibly O(n^2 log n).

---
+++ controller/webapp/management/commands/compare_calculate_workspace_state.py
@@ -0,0 +1,52 @@
+import time
+
+from django.core.management.base import BaseCommand
+from django.db.models import F, Window
+from django.db.models.functions import FirstValue
+
+from controller.queries import calculate_workspace_state
+from controller.webapp.models import Job
+
+
+def time_func(func, *args, **kwargs):
+    start = time.perf_counter()
+    result = func(*args, **kwargs)
+    elapsed = time.perf_counter() - start
+    return result, elapsed
+
+
+def calculate_workspace_state_qs(backend, workspace):
+    included_qs = Job.objects.filter(
+        workspace=workspace, backend=backend, cancelled=False
+    ).exclude(action="__error__")
+    return included_qs.annotate(
+        latest_pk=Window(
+            expression=FirstValue("pk"),
+            partition_by=["action"],
+            order_by=F("created_at").desc(),
+        )
+    ).filter(pk=F("latest_pk"))
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("backend")
+        parser.add_argument("workspace")
+
+    def handle(self, *args, **options):
+        backend = options["backend"]
+        workspace = options["workspace"]
+
+        old_jobs, old_time = time_func(calculate_workspace_state, backend, workspace)
+        new_jobs, new_time = time_func(calculate_workspace_state_qs, backend, workspace)
+
+        self.stdout.write(f"Original: {len(old_jobs)} jobs, {old_time:.4f} sec")
+        self.stdout.write(f"Queryset: {len(new_jobs)}  jobs, {new_time:.4f} sec")
+        self.stdout.write(f"ratio: {old_time / new_time:.1f}")
+
+        old_ids = {job.id for job in old_jobs}
+        new_ids = {job.id for job in new_jobs}
+        if old_ids == new_ids:
+            self.stdout.write(self.style.SUCCESS("Results match"))
+        else:
+            self.stdout.write(self.style.ERROR("Results differ"))
