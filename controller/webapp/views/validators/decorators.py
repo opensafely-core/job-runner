16038bec662becc33e43097b7eff9a910f838e1c
Becky Smith <becky.smith@thedatalab.org>
2025-08-08 09:41:42 +0100

    Validate request.body instead of request.POST

--- controller/webapp/views/validators/decorators.py
+++ controller/webapp/views/validators/decorators.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import JsonResponse
 
 from controller.webapp.views.validators.dataclasses import RequestBody
@@ -13,16 +15,32 @@ def validator(dataclass: RequestBody):
 
     Returns: either
         - the wrapped view, with an additional keyword argument
-        `request_body`, the dataclass instance created from the
-        request POST data.
+        `request_obj`, the dataclass instance created from the
+        posted request.
         - JsonResponse with status 400
     """
 
     def decorator(func):
         def wrapper(*args, **kwargs):
             request = args[0]
+
+            # Django's request.POST is only populated from form data (i.e. content type
+            # application/x-www-form-urlencoded). If we post with content type
+            # application/json, the data will only be in the request body. Since we're
+            # only expected these endpoints to be called with json data, we use the
+            # request.body
             try:
-                obj = dataclass.from_request(request.POST)
+                post_data = json.loads(request.body.decode())
+            except json.JSONDecodeError:
+                return JsonResponse(
+                    {
+                        "error": "Validation error",
+                        "details": "could not parse JSON from request body",
+                    },
+                    status=400,
+                )
+            try:
+                obj = dataclass.from_request(post_data)
             except APIValidationError as e:
                 return JsonResponse(
                     {"error": "Validation error", "details": str(e)}, status=400
