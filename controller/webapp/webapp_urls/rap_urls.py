0fce2364839bde858b3427245ca7686496575d04
Becky Smith <becky.smith@thedatalab.org>
2025-08-18 12:30:06 +0100

    Update urls for API spec and docs

--- controller/webapp/webapp_urls/rap_urls.py
+++ controller/webapp/webapp_urls/rap_urls.py
@@ -6,8 +6,8 @@ from controller.webapp.views import rap_views
 urlpatterns = [
     # RAP API endpoints
     # Documentation
-    path("api-docs/", rap_views.api_docs),
-    path("api_spec.json", rap_views.api_spec),
+    path("docs/", rap_views.api_docs),
+    path("spec.json", rap_views.api_spec),
     # Backend status for all backends (that are allowed for client's token)
     path("backend/status/", rap_views.backends_status, name="backends_status"),
     # Cancel one or more actions for a single job request
