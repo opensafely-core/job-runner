150cf4eb4a5811e6c9b9e4cb4d4e72471e7b5f7f
Dave Evans <dave.evans@thedatalab.org>
2026-01-09 10:55:16 +0000

    Test `handle_task_update()` with every TaskType
    This is to prevent a reoccurrence of the bug introduced in #1301 and
    fixed in #1304.
    
    It's possible this simplistic approach will stop working if we end up
    with TaskTypes that require a specific Task configuration to be handled
    correctly, but we can deal with that when it comes.

--- tests/controller/test_task_api.py
+++ tests/controller/test_task_api.py
@@ -1,3 +1,5 @@
+import pytest
+
 from common.job_executor import JobDefinition
 from controller import task_api
 from controller.main import job_to_job_definition
@@ -40,3 +42,31 @@ def test_mark_inactive(db):
     task_api.mark_task_inactive(task)
     task = task_api.get_task(job.id)
     assert bool(task.active) is False
+
+
+@pytest.mark.parametrize("task_type", list(TaskType))
+@pytest.mark.parametrize("complete", [True, False])
+def test_handle_task_update(db, task_type, complete):
+    task = Task(
+        id="task1",
+        backend="test",
+        type=task_type,
+        definition={"some_key": "some_value"},
+    )
+    task_api.insert_task(task)
+
+    task_api.handle_task_update(
+        task_id="task1",
+        stage="stage1",
+        results={"some_result": "something"},
+        complete=complete,
+    )
+
+    updated_task = task_api.get_task("task1")
+    assert updated_task.active == (not complete)
+    assert updated_task.agent_stage == "stage1"
+    assert updated_task.agent_results == {"some_result": "something"}
+    if complete:
+        assert updated_task.finished_at > 0
+    else:
+        assert updated_task.finished_at is None
